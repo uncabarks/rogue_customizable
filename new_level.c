dd20c42d2bbdddccc247e3686abb612961eabba6
uncabarks <106576618+uncabarks@users.noreply.github.com>
2026-04-10 16:17:49 -0700

    Adjust treasure room parameters in new_level.c

--- new_level.c
+++ new_level.c
@@ -15,9 +15,9 @@
 #include <string.h>
 #include "rogue.h"
 
-#define TREAS_ROOM 20	/* one chance in TREAS_ROOM for a treasure room */
-#define MAXTREAS 10	/* maximum number of treasures in a treasure room */
-#define MINTREAS 2	/* minimum number of treasures in a treasure room */
+#define TREAS_ROOM 5	/* one chance in TREAS_ROOM for a treasure room */
+#define MAXTREAS 20	/* maximum number of treasures in a treasure room */
+#define MINTREAS 8	/* minimum number of treasures in a treasure room */
 
 void
 new_level()
